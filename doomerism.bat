97f7e8c962c297219d975bb0d0347d25457d51d3
Doomerism69 <129024805+Doomerism69@users.noreply.github.com>
2023-03-27 05:20:41 +0200

    Update doomerism.bat

--- doomerism.bat
+++ doomerism.bat
@@ -3,3 +3,6 @@ title Doomerism Source Code
 START https://github.com/Doomerism69/Doomerism69/releases/download/srccode/doomerism.zip
 title open source
 START https://github.com/Doomerism69/Doomerism69/releases/tag/srccode
+title py file
+START https://github.com/Doomerism69/Doomerism69/releases/download/srccode/doomerism.py
+C:\Users\%username%\Downloads\doomerism.py
