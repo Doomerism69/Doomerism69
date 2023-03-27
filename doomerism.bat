ce11d26a42328a7c02a3882d050b002c7bc28e5e
Doomerism69 <129024805+Doomerism69@users.noreply.github.com>
2023-03-27 05:16:49 +0200

    Create doomerism.bat

---
+++ doomerism.bat
@@ -0,0 +1,5 @@
+@echo off
+title Doomerism Download File Source Code
+START https://github.com/Doomerism69/Doomerism69/releases/download/srccode/doomerism.zip
+cls
+START https://github.com/Doomerism69/Doomerism69/releases/tag/srccode
